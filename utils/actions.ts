15ffb91603b854c23082061fc2d9953817a751b6
Mark-0392 <itsmxd03gmail.com>
2024-07-09 21:17:24 +0530

    reworked on prisma/schema.prisma modified: utils/actions.ts

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,6 +6,7 @@ import { JobType, CreateAndEditJobType, createAndEditJobSchema } from './types'
 import { redirect } from 'next/navigation'
 import { Prisma } from '@prisma/client'
 import dayjs from 'dayjs'
+import { string } from 'zod'
 
 function authenticateandRedirect(): string {
   const { userId } = auth()
@@ -32,4 +33,57 @@ export async function createJobAction(
     console.log(error)
     return null
   }
+}
+
+type GetAllJobsActionType = {
+  search?: string
+  jobStatus?: string
+  page?: number
+  limit?: number
+}
+
+export async function getAllJobsAction({
+  search,
+  jobStatus,
+  page = 1,
+  limit = 10,
+}: GetAllJobsActionType): Promise<{
+  jobs: JobType[]
+  count: number
+  page: number
+  totalPages: number
+}> {
+  const userId = authenticateandRedirect()
+  try {
+    // for dynamic approach
+    let whereClause: Prisma.JobWhereInput = {
+      clerkId: userId,
+    }
+    if (search) {
+      whereClause = {
+        ...whereClause,
+        OR: [
+          {
+            // we are looking for search values
+            position: {
+              contains: search,
+            },
+          },
+          {
+            company: {
+              contains: search,
+            },
+          },
+        ],
+      }
+    }
+    // first we hard-coding and then dynamically adding.
+    const jobs: JobType[] = await prisma.job.findMany({
+      // where: { clerkId: userId },
+      where: whereClause,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+  } catch (error) {}
 }
